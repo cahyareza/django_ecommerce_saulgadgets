8513d89b308c18e0df7d65ecc91b1ff8468a5599
cahyafaisalreza <cf.reza07@gmail.com>
2021-06-26 10:07:20 +0700

    get list of products and show on the frontpage

--- saulgadgets_3_8_7/saulgadgets/apps/store/models.py
+++ saulgadgets_3_8_7/saulgadgets/apps/store/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+# Create your models here.
+class Category(models.Model):
+    tittle = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.tittle
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    description = models.TextField(blank=True, null = True)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
