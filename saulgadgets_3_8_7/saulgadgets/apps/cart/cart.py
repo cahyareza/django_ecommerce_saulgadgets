a705484427cd3cfa15b09fea68a65a34218df09f
cahyafaisalreza <cf.reza07@gmail.com>
2021-06-30 16:37:58 +0700

    create a cart class

---
+++ saulgadgets_3_8_7/saulgadgets/apps/cart/cart.py
@@ -0,0 +1,49 @@
+from django.conf import settings
+
+
+class Cart(object):
+    '''
+    Akan membuat session dengan keys cart dan salah satu valuenya product_id
+    "cart" ={
+    "product_id": {
+        'quantity': 0,
+        'price': $9.99,
+        'id': nike
+        }
+    }
+
+    Kemudian pada session ini bisa menambahkan di session cart menggunakan prinsip2 dictionary data type
+    '''
+
+    def __init__(self):
+        self.session = request.session
+        # initialize session cart
+        cart = self.session.get(settings.CART_SESSION_ID)
+
+        # jika tidak bukan cart
+        if not cart:
+            cart = self.session[settings.CART_SESSION_ID] = {}
+
+        self.cart = cart
+
+    def add(self, product, quantity =1, update_quantity=False):
+        product_id = str(product.id)
+        price = str(product.price)
+
+        #jika pada session cart tidak ada keys product_id maka dibuat
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price':price, 'id': product_id}
+
+        #update quantity
+        if update_quantity:
+            self.cart[product_id]["quantity"] = quantity
+
+        else:
+            self.cart[product_id]["quantity"] = self.cart[product_id]["quantity"] + 1
+
+        self.save()
+
+    def save(self):
+        # update session
+        self.session[settings.CART_SESSION_ID] = self.cart
+        self.session.modified = True
